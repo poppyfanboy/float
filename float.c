0dcb0c281a7b636367afd32966f865aeb9530017
poppyfanboy <64432114+poppyfanboy@users.noreply.github.com>
2025-08-07 03:32:21 +0300

    Handle subnormals and special values

--- float.c
+++ float.c
@@ -1,44 +1,69 @@
 #include "float.h"
 
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <string.h> // memcpy, memset, strlen, strcpy
+#include <stdlib.h> // abort
+#include <assert.h> // assert
 
 typedef struct {
-    char *data;
-    isize size;
-    isize capacity;
-} String;
+    u8 *begin;
+    u8 *end;
+} Arena;
 
-void string_push_char(String *string, char value) {
-    if (string->size + 1 >= string->capacity) {
-        isize new_capacity = 2 * string->capacity;
-        if (new_capacity < 16) {
-            new_capacity = 16;
+#define ARENA_ALIGNMENT 16
+
+static void *arena_alloc(void *arena_void, isize size) {
+    Arena *arena = arena_void;
+
+    isize padding = (~(uptr)arena->begin + 1) & (ARENA_ALIGNMENT - 1);
+    assert(arena->begin + padding + size <= arena->end);
+
+    void *ptr = arena->begin + padding;
+    arena->begin += padding + size;
+    return ptr;
+}
+
+typedef struct {
+    char *dest;
+    isize dest_size;
+    isize chars_written;
+} StringWriter;
+
+static void string_writer_push_char(StringWriter *string_writer, char next_char) {
+    if (string_writer->dest != NULL) {
+        if (string_writer->chars_written == string_writer->dest_size) {
+            abort();
         }
-        string->data = realloc(string->data, new_capacity);
-        string->capacity = new_capacity;
-        string->data[string->size] = 0;
+        string_writer->dest[string_writer->chars_written] = next_char;
     }
-
-    string->data[string->size++] = value;
-    string->data[string->size] = 0;
+    string_writer->chars_written += 1;
 }
 
-void string_reverse(String *string) {
-    for (isize i = 0; i < string->size / 2; i += 1) {
-        char swap = string->data[i];
-        string->data[i] = string->data[string->size - i - 1];
-        string->data[string->size - i - 1] = swap;
+static void string_writer_push_string(StringWriter *string_writer, char const *next_string) {
+    isize next_string_size = strlen(next_string);
+
+    if (string_writer->dest != NULL) {
+        if (string_writer->chars_written + next_string_size > string_writer->dest_size) {
+            abort();
+        }
+        strcpy(string_writer->dest + string_writer->chars_written, next_string);
+    } else {
+        string_writer->chars_written += next_string_size;
     }
 }
 
-typedef struct {
-    bool is_negative;
-    isize exponent;
-    u32 *mantissa;
-    isize mantissa_size;
-} Float;
+static void string_reverse(char *begin, char *end) {
+    char *forwards_iter = begin;
+    char *backwards_iter = end - 1;
+
+    while (forwards_iter < backwards_iter) {
+        char swap = *forwards_iter;
+        *forwards_iter = *backwards_iter;
+        *backwards_iter = swap;
+
+        forwards_iter += 1;
+        backwards_iter -= 1;
+    }
+}
 
 static bool is_zero(u32 *number, isize number_size) {
     for (isize i = 0; i < number_size; i += 1) {
@@ -54,22 +79,19 @@ static void divide(
     u32 *dividend,
     isize dividend_size,
     u32 divisor,
-    u32 *result,
-    u32 *remainder
+    u32 *quotient,
+    u32 *final_remainder
 ) {
-    u32 *result_iter = result;
-
-    u32 carry = 0;
+    u32 remainder = 0;
     for (isize i = 0; i < dividend_size; i += 1) {
-        u64 dividend_small = ((u64)carry << 32) | dividend[i];
+        u64 dividend_chunk = ((u64)remainder << 32) | dividend[i];
 
-        // Result should fit into u32 but I don't know why
-        *(result++) = dividend_small / divisor;
-
-        carry = dividend_small % divisor;
+        // Result should fit into u32, but I don't know why.
+        quotient[i] = dividend_chunk / divisor;
+        remainder = dividend_chunk % divisor;
     }
 
-    *remainder = carry;
+    *final_remainder = remainder;
 }
 
 static void multiply(
@@ -77,46 +99,35 @@ static void multiply(
     isize multiplicand_size,
     u32 multiplier,
     u32 *product,
-    u32 *final_carry
+    u32 *final_overflow
 ) {
-    u32 *product_iter = product + multiplicand_size - 1;
-
-    u32 carry = 0;
+    u32 overflow = 0;
     for (isize i = multiplicand_size - 1; i >= 0; i -= 1) {
-        u64 product_small = (u64)carry + (u64)multiplier * (u64)multiplicand[i];
+        u64 product_chunk = (u64)overflow + (u64)multiplier * (u64)multiplicand[i];
 
-        *(product_iter--) = product_small & 0xffffffff;
-
-        carry = product_small >> 32;
+        product[i] = product_chunk & 0xffffffff;
+        overflow = product_chunk >> 32;
     }
 
-    *final_carry = carry;
+    *final_overflow = overflow;
 }
 
+typedef struct {
+    bool is_negative;
+    isize exponent;
+    u32 *mantissa;
+    isize mantissa_size;
+} Float;
+
+// Mantissa of the Float value here is expected to either start with a 1 or be completely zeroed.
+// Which means that when the exponent is 0 the absolute value encoded into the float is less than 1.
 static inline isize float_format(
     Float value,
-    char *string,
-    isize string_size,
-    FloatFormatParams const *params
+    char *string, isize string_size,
+    FloatFormatParams const *params,
+    Arena arena
 ) {
-    if (string != NULL) {
-        printf("is_negative: %s\n", value.is_negative ? "yes" : "no");
-        printf("exponent: %lld\n", value.exponent);
-
-        printf("mantissa: ");
-        for (isize i = 0; i < value.mantissa_size; i += 1) {
-            for (isize j = 0; j < 32; j += 1) {
-                printf("%d", (value.mantissa[i] >> (31 - j)) & 1);
-            }
-        }
-        printf("\n");
-    }
-
-    if (string != NULL && string_size > 0) {
-        string[0] = 0;
-    }
-
-    // Size in u32-s
+    // Sizes are measured in u32s count.
     isize integer_part_size;
     isize fractional_part_size;
     if (value.exponent == 0) {
@@ -124,7 +135,7 @@ static inline isize float_format(
         fractional_part_size = value.mantissa_size;
     } else if (value.exponent < 0) {
         integer_part_size = 1;
-        fractional_part_size += ((-value.exponent) + 31) / 32;
+        fractional_part_size = ((-value.exponent) + 31) / 32;
     } else if (value.exponent > 0) {
         integer_part_size = (value.exponent + 31) / 32;
 
@@ -135,7 +146,9 @@ static inline isize float_format(
         }
     }
 
-    u32 *float_data = calloc(integer_part_size + fractional_part_size, sizeof(u32));
+    u32 *float_data = arena_alloc(&arena, (integer_part_size + fractional_part_size) * sizeof(u32));
+    memset(float_data, 0, (integer_part_size + fractional_part_size) * sizeof(u32));
+
     u32 *integer_part = float_data;
     u32 *fractional_part = integer_part + integer_part_size;
 
@@ -163,117 +176,206 @@ static inline isize float_format(
         }
     }
 
-    if (string != NULL) {
-        printf("Float expanded:\n");
-        for (isize i = 0; i < integer_part_size; i += 1) {
-            for (isize j = 0; j < 32; j += 1) {
-                printf("%d", (integer_part[i] >> (31 - j)) & 1);
-            }
-        }
-        printf(".");
-        for (isize i = 0; i < fractional_part_size; i += 1) {
-            for (isize j = 0; j < 32; j += 1) {
-                printf("%d", (fractional_part[i] >> (31 - j)) & 1);
-            }
-        }
-        printf("\n");
+    StringWriter string_writer = {
+        .dest = string,
+        .dest_size = string_size,
+        .chars_written = 0,
+    };
+
+    if (value.is_negative) {
+        string_writer_push_char(&string_writer, '-');
     }
 
-    String integer_part_decimal = {0};
+    if (is_zero(integer_part, integer_part_size)) {
+        string_writer_push_char(&string_writer, '0');
+    } else {
+        char *string_integer_part_begin = string + string_writer.chars_written;
 
-    u32 *quotient = malloc(integer_part_size * sizeof(u32));
-    while (!is_zero(integer_part, integer_part_size)) {
-        u32 remainder;
-        divide(integer_part, integer_part_size, 10, quotient, &remainder);
+        u32 *quotient = arena_alloc(&arena, integer_part_size * sizeof(u32));
+        while (!is_zero(integer_part, integer_part_size)) {
+            u32 remainder;
+            divide(integer_part, integer_part_size, 10, quotient, &remainder);
 
-        string_push_char(&integer_part_decimal, '0' + remainder);
+            string_writer_push_char(&string_writer, '0' + remainder);
 
-        u32 *swap = quotient;
-        quotient = integer_part;
-        integer_part = swap;
+            u32 *swap = quotient;
+            quotient = integer_part;
+            integer_part = swap;
+        }
+
+        if (string != NULL) {
+            string_reverse(string_integer_part_begin, string + string_writer.chars_written);
+        }
     }
 
-    String fractional_part_decimal = {0};
+    string_writer_push_char(&string_writer, '.');
 
     if (is_zero(fractional_part, fractional_part_size)) {
-        string_push_char(&fractional_part_decimal, '0');
+        string_writer_push_char(&string_writer, '0');
+    } else {
+        u32 *product = arena_alloc(&arena, fractional_part_size * sizeof(u32));
+        while (!is_zero(fractional_part, fractional_part_size)) {
+            u32 overflow;
+            multiply(fractional_part, fractional_part_size, 10, product, &overflow);
+
+            string_writer_push_char(&string_writer, '0' + overflow);
+
+            u32 *swap = product;
+            product = fractional_part;
+            fractional_part = swap;
+        }
     }
 
-    u32 *product = malloc(fractional_part_size * sizeof(u32));
-    while (!is_zero(fractional_part, fractional_part_size)) {
-        u32 overflow;
-        multiply(fractional_part, fractional_part_size, 10, product, &overflow);
-
-        string_push_char(&fractional_part_decimal, '0' + overflow);
-
-        u32 *swap = product;
-        product = fractional_part;
-        fractional_part = swap;
-    }
-
-    if (string != NULL) {
-        string_reverse(&integer_part_decimal);
-        printf("%s.%s\n", integer_part_decimal.data, fractional_part_decimal.data);
-    }
-
-    return 1;
+    string_writer_push_char(&string_writer, '\0');
+    return string_writer.chars_written;
 }
 
-isize f32_format(
-    f32 value,
-    char *string,
-    isize string_size,
-    FloatFormatParams const *params
-) {
+isize f32_format(f32 value, char *string, isize string_size, FloatFormatParams const *params) {
     u32 raw_value;
     memcpy(&raw_value, &value, 4);
 
     bool is_negative = (raw_value & 0x80000000) != 0;
     raw_value &= 0x7fffffff;
 
-    int mantissa_bit_size = 23;
+    // Mantissa size without an implicit 1.
+    int const mantissa_bits = 23;
+    u32 const mantissa_mask = 0x007fffff;
 
-    // TODO: Handle denormalized floats
-    isize exponent = (isize)(raw_value >> mantissa_bit_size) - 127 + 1;
-    u32 mantissa = (raw_value & 0x007fffff) << (32 - mantissa_bit_size);
+    isize raw_exponent = (isize)(raw_value >> mantissa_bits);
 
-    // Add an implicit 1
-    mantissa = 0x80000000 | (mantissa >> 1);
+    isize exponent;
+    u32 mantissa;
+    if (raw_exponent == 0) {
+        // Subnormals and zero.
+
+        exponent = -126;
+        mantissa = (raw_value & mantissa_mask) << (32 - mantissa_bits);
+
+        if (mantissa != 0) {
+            int leading_zeroes = __builtin_clz(mantissa);
+            mantissa <<= leading_zeroes;
+            exponent -= leading_zeroes;
+        }
+    } else if (raw_exponent == 255) {
+        // Infinities and NaNs.
+
+        StringWriter string_writer = {
+            .dest = string,
+            .dest_size = string_size,
+            .chars_written = 0,
+        };
+
+        mantissa = raw_value & mantissa_mask;
+        if (mantissa == 0) {
+            if (is_negative) {
+                string_writer_push_char(&string_writer, '-');
+            }
+            string_writer_push_string(&string_writer, "inf");
+        } else {
+            string_writer_push_string(&string_writer, "nan");
+        }
+
+        return string_writer.chars_written;
+    } else {
+        // Normal floats.
+
+        exponent = raw_exponent - 127;
+        mantissa = (raw_value & mantissa_mask) << (32 - mantissa_bits);
+
+        // Add an implicit 1.
+        exponent += 1;
+        mantissa = 0x80000000 | (mantissa >> 1);
+    }
+
+    // Should be enough memory to convert any f32 float?
+    u8 arena_buffer[8 * 1024];
+    Arena arena = {
+        .begin = arena_buffer,
+        .end = arena_buffer + 8 * 1024,
+    };
 
     return float_format(
-        (Float){is_negative, exponent, &mantissa, .mantissa_size = 1},
-        string,
-        string_size,
-        params
+        (Float){
+            is_negative, exponent, (u32[]){mantissa},
+            .mantissa_size = 1,
+        },
+        string, string_size,
+        params,
+        arena
     );
 }
 
-isize f64_format(
-    f64 value,
-    char *string,
-    isize string_size,
-    FloatFormatParams const *params
-) {
+isize f64_format(f64 value, char *string, isize string_size, FloatFormatParams const *params) {
     u64 raw_value;
     memcpy(&raw_value, &value, 8);
 
     bool is_negative = (raw_value & 0x8000000000000000) != 0;
     raw_value &= 0x7fffffffffffffff;
 
-    // TODO: Handle denormalized floats
-    isize exponent = (isize)(raw_value >> 52) - 1023 + 1;
-    u32 mantissa[2] = {
-        // Add an implicit 1
-        // 1 set implicit bit + 31 bit from mantissa goes into the first element
-        0x80000000 | ((raw_value >> (52 - 31)) & 0x7fffffff),
-        // the rest 21 bit go into the second element
-        (raw_value & 0x001fffff) << 11,
+    // Mantissa size without an implicit 1.
+    int const mantissa_bits = 52;
+    u64 const mantissa_mask = 0xfffffffffffff;
+
+    isize raw_exponent = (isize)(raw_value >> mantissa_bits);
+
+    isize exponent;
+    u64 mantissa;
+    if (raw_exponent == 0) {
+        // Subnormals and zero.
+
+        exponent = -1022;
+        mantissa = (raw_value & mantissa_mask) << (64 - mantissa_bits);
+
+        if (mantissa != 0) {
+            int leading_zeroes = __builtin_clzll(mantissa);
+            mantissa <<= leading_zeroes;
+            exponent -= leading_zeroes;
+        }
+    } else if (raw_exponent == 2047) {
+        // Infinities and NaNs.
+
+        StringWriter string_writer = {
+            .dest = string,
+            .dest_size = string_size,
+            .chars_written = 0,
+        };
+
+        mantissa = raw_value & mantissa_mask;
+        if (mantissa == 0) {
+            if (is_negative) {
+                string_writer_push_char(&string_writer, '-');
+            }
+            string_writer_push_string(&string_writer, "inf");
+        } else {
+            string_writer_push_string(&string_writer, "nan");
+        }
+
+        return string_writer.chars_written;
+    } else {
+        // Normal floats.
+
+        exponent = raw_exponent - 1023;
+        mantissa = (raw_value & mantissa_mask) << (64 - mantissa_bits);
+
+        // Add an implicit 1.
+        exponent += 1;
+        mantissa = 0x8000000000000000 | (mantissa >> 1);
+    }
+
+    // Should be enough memory to convert any f64 float?
+    u8 arena_buffer[8 * 1024];
+    Arena arena = {
+        .begin = arena_buffer,
+        .end = arena_buffer + 8 * 1024,
     };
 
     return float_format(
-        (Float){is_negative, exponent, mantissa, .mantissa_size = 2},
-        string,
-        string_size,
-        params
+        (Float){
+            is_negative, exponent, (u32[]){mantissa >> 32, mantissa & 0xffffffff},
+            .mantissa_size = 2,
+        },
+        string, string_size,
+        params,
+        arena
     );
 }
--- main.c
+++ main.c
@@ -1,27 +1,61 @@
 #include "float.h"
 
+#include <float.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-    f32 f32_input_value = 123.7654321F;
-    {
-        isize buffer_size = f32_format(f32_input_value, NULL, 0, &(FloatFormatParams){.precision = 0});
-        char *buffer = malloc(buffer_size);
-        f32_format(f32_input_value, buffer, buffer_size, &(FloatFormatParams){.precision = 0});
+void f32_print(f32 value, FloatFormatParams *params, char const *label) {
+    isize buffer_size = f32_format(value, NULL, 0, params);
+    char *buffer = malloc(buffer_size);
+    f32_format(value, buffer, buffer_size, params);
+    printf("%s = %s\n", label, buffer);
+    free(buffer);
+}
 
-        printf("f32_input_value = %s\n", buffer);
+void f64_print(f64 value, FloatFormatParams *params, char const *label) {
+    isize buffer_size = f64_format(value, NULL, 0, params);
+    char *buffer = malloc(buffer_size);
+    f64_format(value, buffer, buffer_size, params);
+    printf("%s = %s\n", label, buffer);
+    free(buffer);
+}
+
+int main(void) {
+    {
+        FloatFormatParams params = {.precision = 0};
+        f32_print(0.0F, &params, "f32 zero");
+        f32_print(123.456F, &params, "regular positive f32 (123.456F)");
+        f32_print(-4312.12F, &params, "regular negative f32 (-4312.12F)");
+        f32_print(128.125F, &params, "exact f32 (128.125F)");
+        f32_print(FLT_MIN, &params, "min f32");
+        f32_print(FLT_MAX, &params, "max f32");
+
+        union { u32 i; f32 f; } denormalized = {.i = 1};
+        f32_print(denormalized.f, &params, "denormalized f32");
+
+        f32_print(nanf(""), &params, "NaN f32");
+        f32_print(INFINITY, &params, "positive infinity f32");
+        f32_print(-INFINITY, &params, "negative infinity f32");
     }
 
     printf("\n");
 
-    f64 f64_input_value = 1e100;
     {
-        isize buffer_size = f64_format(f64_input_value, NULL, 0, &(FloatFormatParams){.precision = 0});
-        char *buffer = malloc(buffer_size);
-        f64_format(f64_input_value, buffer, buffer_size, &(FloatFormatParams){.precision = 0});
+        FloatFormatParams params = {.precision = 0};
+        f64_print(0.0, &params, "f64 zero");
+        f64_print(1234567.7654321, &params, "regular positive f64 (1234567.7654321)");
+        f64_print(-7654321.1234567, &params, "regular negative f64 (-7654321.1234567)");
+        f64_print(128.125, &params, "exact f64 (128.125)");
+        f64_print(DBL_MIN, &params, "min f64");
+        f64_print(DBL_MAX, &params, "max f64");
 
-        printf("f64_input_value = %s\n", buffer);
+        union { u64 i; f64 f; } denormalized = {.i = 1};
+        f64_print(denormalized.f, &params, "denormalized f64");
+
+        f64_print(nan(""), &params, "NaN f64");
+        f64_print(INFINITY, &params, "positive infinity f64");
+        f64_print(-INFINITY, &params, "negative infinity f64");
     }
 
     return 0;
